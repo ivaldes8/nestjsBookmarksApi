b781b1a23ddd62260a9fd74d8dafdab66420be67
Ivan Gonzalez Valdes <kaosolution8@gmail.com>
2022-11-27 15:26:26 -0500

    first commit

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,11 @@
+import { IsEAN, IsEmail, IsNotEmpty, IsString } from "class-validator"
+
+export class AuthDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,23 @@
+import { EditUserDto } from './dto/edit-user.dto';
+import { JwtGuard } from './../auth/guard/jwt.guard';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
+import { UserService } from './user.service';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+    constructor(private userService: UserService) { }
+
+    @Get('me')
+    getMe(@GetUser() user: User) {
+        return user;
+    }
+
+    @Patch()
+    editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+        return this.userService.editUser(userId, dto)
+    }
+}
